7b63f71c0884dbdce51d530584c525ec8c2dac72
jan <jan@DESKTOP-8UN8QGM>
2017-04-25 11:49:25 +0200

    Pierwsza wersja

---
+++ src/mojpakiet/Oblicz.java
@@ -0,0 +1,11 @@
+
+package mojpakiet;
+
+public class Oblicz {
+    
+    public int dodaj(int x,int y)
+    {
+        return x+y;
+    }
+    
+}
---
+++ src/mojpakiet/Run.java
@@ -0,0 +1,12 @@
+
+package mojpakiet;
+
+public class Run {
+
+    public static void main(String[] args) {
+        Oblicz o = new Oblicz();
+        int w = o.dodaj(2, 3);
+        System.out.println("Wynik=" + w);
+    }
+
+}
